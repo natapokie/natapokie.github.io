1f002e979e7362d0176b0a646c6adfee1fd48efa
Natalie Chan <natapoke2@gmail.com>
2025-01-13 15:16:25 -0500

    adding mousemove event tracking to move iris

--- my-app/app/components/avatar/avatar.tsx
+++ my-app/app/components/avatar/avatar.tsx
@@ -1,28 +1,55 @@
-import Image from 'next/image';
-import styles from './avatar.module.css';
-import base from '../../../public/avatar/base.svg';
-import shirt from '../../../public/avatar/shirt.svg';
-import hair1 from '../../../public/avatar/hair-1.svg';
-import bangs1 from '../../../public/avatar/bangs-1.svg';
-import sides from '../../../public/avatar/side.svg';
-import eyeRightOpen from '../../../public/avatar/eye-r-open.svg';
-import eyeLeftOpen from '../../../public/avatar/eye-l-open.svg';
-import smile from '../../../public/avatar/smile.svg';
-import iris from '../../../public/avatar/iris.svg';
-import eyelashRightOpen from '../../../public/avatar/eyelash-r-open.svg';
-import eyelashLeftOpen from '../../../public/avatar/eyelash-l-open.svg';
+"use client";
+
+import Image from "next/image";
+import styles from "./avatar.module.css";
+import base from "../../../public/avatar/base.svg";
+import shirt from "../../../public/avatar/shirt.svg";
+import hair1 from "../../../public/avatar/hair-1.svg";
+import bangs1 from "../../../public/avatar/bangs-1.svg";
+import sides from "../../../public/avatar/side.svg";
+import eyeRightOpen from "../../../public/avatar/eye-r-open.svg";
+import eyeLeftOpen from "../../../public/avatar/eye-l-open.svg";
+import smile from "../../../public/avatar/smile.svg";
+import iris from "../../../public/avatar/iris.svg";
+import eyelashRightOpen from "../../../public/avatar/eyelash-r-open.svg";
+import eyelashLeftOpen from "../../../public/avatar/eyelash-l-open.svg";
+import { useEffect, useState } from "react";
 
 export const Avatar = () => {
   return (
     <>
       <div className="relative origin-center w-[674px] h-[1021.6px]">
-        <Image src={hair1} alt="hair" className={`absolute z-[-1] ${styles.hair1}`}></Image>
-        <Image src={base} alt="base" className={`absolute ${styles.base}`}></Image>
+        <Image
+          src={hair1}
+          alt="hair"
+          className={`absolute z-[-1] ${styles.hair1}`}
+        ></Image>
+        <Image
+          src={base}
+          alt="base"
+          className={`absolute ${styles.base}`}
+        ></Image>
 
-        <Image src={shirt} alt="shirt" className={`absolute ${styles.shirt}`}></Image>
-        <Image src={sides} alt="sides" className={`absolute ${styles.sides}`}></Image>
-        <Image src={bangs1} alt="bangs" className={`absolute ${styles.bangs1}`}></Image>
-        <Image src={smile} alt="smile" className={`absolute ${styles.smile}`}></Image>
+        <Image
+          src={shirt}
+          alt="shirt"
+          className={`absolute ${styles.shirt}`}
+        ></Image>
+        <Image
+          src={sides}
+          alt="sides"
+          className={`absolute ${styles.sides}`}
+        ></Image>
+        <Image
+          src={bangs1}
+          alt="bangs"
+          className={`absolute ${styles.bangs1}`}
+        ></Image>
+        <Image
+          src={smile}
+          alt="smile"
+          className={`absolute ${styles.smile}`}
+        ></Image>
 
         <div className="absolute top-[254.4px] left-[94.18px]">
           <Eye></Eye>
@@ -33,6 +60,30 @@ export const Avatar = () => {
 };
 
 const Eye = () => {
+  const [position, setPosition] = useState({ x: "49.77px", y: "124.07px" });
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+
+      const scaleX = event.pageX / window.outerWidth;
+      const scaleY = event.pageY / window.outerHeight;
+
+      const newPositionX = Math.max(30, Math.min(scaleX * 162.92, 70));
+      const newPositionY = Math.max(114, Math.min(scaleY * 234.58, 134));
+
+      setPosition({
+        x: `${newPositionX}px`,
+        y: `${newPositionY}px`,
+      });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove); // Cleanup
+    };
+  }, []);
+
   return (
     <>
       <div className="relative w-[162.92px] h-[234.58px]">
@@ -41,13 +92,21 @@ const Eye = () => {
           alt="eye-left"
           className={`absolute ${styles.eyeLeftOpen}`}
         ></Image>
-        <Image src={iris} alt="iris-left" className={`absolute ${styles.irisLeft}`}></Image>
+        <Image
+          src={iris}
+          alt="iris-left"
+          className={`absolute ${styles.irisLeft}`}
+          style={{ top: position.y, left: position.x }}
+        ></Image>
         <Image
           src={eyelashLeftOpen}
           alt="eyelash-left"
           className={`absolute ${styles.eyelashLeftOpen}`}
         ></Image>
       </div>
+      {position.x}
+      {"\n"}
+      {position.y}
     </>
   );
 };
