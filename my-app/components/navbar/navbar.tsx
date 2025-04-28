2549a186ed9d3254d500bd9795de2675643bbfb0
Natalie Chan <natapoke2@gmail.com>
2025-04-27 23:27:40 -0400

    navigate on nav icon click

--- my-app/components/navbar/navbar.tsx
+++ my-app/components/navbar/navbar.tsx
@@ -3,15 +3,27 @@
 import { NavbarItems } from "@/lib/static/intro";
 import styles from "./navbar.module.css";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Navbar() {
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const pathname = usePathname();
+  const router = useRouter();
 
   const handleNavItemClick = (index: number) => {
-    console.log("Clicked on route:", index);
     setSelectedIndex(index);
+    router.push(NavbarItems[index].route);
   };
+
+  useEffect(() => {
+    const index = NavbarItems.findIndex((item) => item.route === pathname);
+    if (index !== -1) {
+      setSelectedIndex(index);
+    } else {
+      setSelectedIndex(0); // Default to the first item if not found
+    }
+  }, [pathname]);
 
   return (
     <div className="flex flex-row justify-start items-center">
